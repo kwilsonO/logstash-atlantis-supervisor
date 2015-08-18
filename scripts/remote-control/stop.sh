1fe8ff9c5c2ebdc7b82390bb90c45f1b429fb6b5
Kyle Wilson <kwilson@ooyala.com>
2015-08-18 15:15:53 -0700

    quotes

--- scripts/remote-control/stop.sh
+++ scripts/remote-control/stop.sh
@@ -1,5 +1,5 @@
 echo "Stopping logstash..."
-myid="$(ps -ef | grep "logstash-1.5.3" | grep -v grep | awk '{print $2}')"
+myid="$(ps -ef | grep 'logstash-1.5.3' | grep -v grep | awk '{print $2}')"
 
 if [ "${myid}" = "" ]; then 
 
